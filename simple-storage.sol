0c054f2807d122804be9a0d34589e7256a5b3384
Sreeram V Gopal <sreeramvg100@gmail.com>
2025-10-18 21:27:00 +0530

    first smart contract

---
+++ simple-storage.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.30;
+
+contract SimpleStorage{
+    uint public favnumber=5;
+    bool favbool=false;
+    string favstring="Sreeram";
+    address favaddress=0xDFDa8340978B38d93114FAE615144e895A75ebb2;
+
+    function store(uint number) public 
+    {
+        favnumber=number;
+    }
+
+
+    struct People
+    {
+        uint integer;
+        string name;
+    }
+
+    //People public person=People({integer:1,name:"Archana"});
+    People[] public peoplearray;
+
+    //mapping
+    mapping(string=>uint) public mappingname;
+
+    //using memory in variable parameter makes it so that the value will be stored only during the execution of the program
+    function addperson(uint functioninteger, string memory functionname) public
+    {
+        peoplearray.push(People({integer: functioninteger, name:functionname})); 
+        mappingname[functionname]=functioninteger;
+    }
+}
