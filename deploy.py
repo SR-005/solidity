31727e5229d9de13802848fdd2142026f7ea491d
Sreeram V Gopal <sreeramvg100@gmail.com>
2025-10-19 23:52:10 +0530

    compilation completed

--- deploy.py
+++ deploy.py
@@ -1,4 +1,21 @@
+from solcx import compile_standard,install_solc
+install_solc("0.8.30")
 
 #Read the Smart Contract
-with open("./simple-storage","r") as file:
+with open("./simple-storage.sol","r") as file:
     simplestoragefile=file.read()
+
+
+compiledsol = compile_standard(
+    {
+        "language":"Solidity",
+        "sources":{"simple-storage.sol":{"content":simplestoragefile}},
+        "settings":{
+            "outputSelection":  {
+                "*":{"*": ["abi","metadata","evm.bytecode","evm.sourceMap"]}
+            }
+        }
+    },solc_version="0.8.30"
+)
+
+print(compiledsol)
