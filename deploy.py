7456a9e8471ca263521c38f488ca2ebb7d4f4270
Sreeram V Gopal <sreeramvg100@gmail.com>
2025-10-21 19:19:44 +0530

    created contract

--- deploy.py
+++ deploy.py
@@ -1,6 +1,13 @@
 import json
+import os
+from dotenv import load_dotenv
 from solcx import compile_standard,install_solc
+from web3 import Web3
 install_solc("0.8.30")
+
+load_dotenv()
+MYADDRESS = os.getenv("MYADDRESS")
+SECRETCODE = os.getenv("SECRETKEY")
 
 #Read the Smart Contract
 with open("./simple-storage.sol","r") as file:
@@ -29,4 +36,10 @@ bytecode=compiledsol["contracts"]["simple-storage.sol"]["SimpleStorage"]["evm"][
 #get abi from the compiled Smart Contract
 abi=compiledsol["contracts"]["simple-storage.sol"]["SimpleStorage"]["abi"]
 
-print("ABI: ",abi)
+#connecting to Ganache Blockchain
+w3=Web3(Web3.HTTPProvider("HTTP://127.0.0.1:7545"))
+chainid=5777
+
+#creating the contract
+SimpleStorage=w3.eth.contract(abi=abi,bytecode=bytecode)
+print(SimpleStorage)
