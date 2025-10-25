4e6ac896d320aa0f25ada857fe2248768c103f21
Sreeram V Gopal <sreeramvg100@gmail.com>
2025-10-25 14:58:50 +0530

    interacting with smart contract- brownie

--- brownie/scripts/deploy-simplestorage.py
+++ brownie/scripts/deploy-simplestorage.py
@@ -1,10 +1,27 @@
-from brownie import accounts
+from brownie import accounts, SimpleStorage, network
+import os,time
 
 def deploysimplestorage():
-    '''account=accounts[0]'''
-    account=accounts.load("metamask")
+    account=accounts[0]   #for default brownie ganache account
+
+    '''account=accounts.load("metamask")'''     #one of the method for loading external account --but need to enter passwrd each time 
+    '''account=accounts.add(os.getenv("METAMASK_SECRETKEY"))'''   #fetching the account from .env variable
+
     print(account)
+    simplestorage=SimpleStorage.deploy({"from":account})  
+    #simple call function- no transaction  
+    value=simplestorage.retrieve()
+    print("Old Value: ",value)
+
+    #transaction function
+    transaction=simplestorage.store(15, {"from":account})
+    transaction.wait(1)     #wait for transaction block to confirm
+    value=simplestorage.retrieve()
+    print("New Value: ",value)
+
 
 def main():
     print("hello sm")
     deploysimplestorage()
+    time.sleep(2)
+    network.disconnect()    #for a clean connection termination
