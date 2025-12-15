5fd96327432a158a13a7b37e9df606925c423c44
Sreeram V Gopal <sreeramvg100@gmail.com>
2025-12-15 21:48:06 +0530

    deployable nft contract

--- simplenft/simplenft.sol
+++ simplenft/simplenft.sol
@@ -17,7 +17,6 @@ contract SimpleNFT is ERC721URIStorage, Ownable
     {
         
         uint newtokenid=tokencounter;
-        string memory tokenuri="ipfs://Qmd9MCGtdVz2miNumBHDbvj8bigSgTwnr4SbyH6DNnpWdt?filename=0-PUG.json";
         _safeMint(msg.sender, newtokenid);
         _setTokenURI(newtokenid, tokenuri);
         tokencounter=tokencounter+1; 
